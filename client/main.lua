3dd4ff4daa06506127a670f7c658eceac7a2fe90
Miterosan <miterosan@gmail.com>
2024-01-14 21:08:06 +0100

    Add the lua mod script

---
+++ client/main.lua
@@ -0,0 +1,35 @@
+local mod = RegisterMod("Shocking", 1)
+local socket = require("socket")
+
+
+
+
+function mod:onIsaacDamage(aEntity ,DamageAmount ,DamageFlags ,DamageSource ,DamageCountdown)
+
+    local host, port = "localhost", 11000
+    local tcp = assert(socket.tcp())
+
+    tcp:connect(host, port)
+
+    local isIntentional = (DamageFlags & DamageFlag.DAMAGE_NO_PENALTIES) == DamageFlag.DAMAGE_NO_PENALTIES
+
+
+    local amount = tonumber(string.format(DamageAmount, "%.f"))
+
+    if isIntentional then
+        tcp:send("onIntentionalDamage," .. tostring(amount))
+    else
+        tcp:send("onDamage," .. tostring(amount))
+    end
+
+
+    --tcp:close()
+
+    return nil
+
+
+end
+
+
+
+mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod.onIsaacDamage, EntityType.ENTITY_PLAYER)
